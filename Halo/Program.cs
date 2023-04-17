f28058447c85575dd06d762570d6c10b65512c8d
bartl <bartl@VIAPOL>
2023-04-17 17:19:02 +0200

    Add project files.

---
+++ Halo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("nazywam się Bartłomiej Mazur");
+    }
+}
